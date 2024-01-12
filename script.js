13832699695e82f1eb7c02f123874ecc998d2bdd
Amaan Jaweed <amaanmujtaba@gmail.com>
2024-01-12 00:00:54 -0500

    Scroll done, Website set up, services done

---
+++ script.js
@@ -0,0 +1,125 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+
+const nav = document.querySelector('.nav');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+/*
+const btnScrollTo = document.querySelector(".btn--text.btn--scroll-to");
+
+btnScrollTo.addEventListener('click', function(e){
+  alert("Button clicked");
+});
+*/
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+btnScrollTo.addEventListener('click', function(e){
+  e.preventDefault();
+  //alert("Button clicked");
+  section1.scrollIntoView( {behavior: 'smooth'}) ;
+
+});
+
+// Tabbed components: Services
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+//Menu fade
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+
+//Revealing elements
+const sections = document.querySelectorAll('.section');
+
+const obsCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry.isIntersecting);
+
+    if (entry.isIntersecting === true) {
+      entry.target.classList.remove('section--hidden');
+    } else {
+      entry.target.classList.add('section--hidden');
+    }
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.1,
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+
+sections.forEach(function (section) {
+  observer.observe(section);
+});
+ //section 1 is the object being observed
+
+
+
